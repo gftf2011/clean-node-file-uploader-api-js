e8e5e59545e44431c8141156af8c8a7fc35f6a3f
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-01-09 01:03:34 -0300

    test: ensure FileRouter returns 400 if no filenameis provided

--- tests/presentation/router/file-router.spec.js
+++ tests/presentation/router/file-router.spec.js
@@ -6,10 +6,23 @@ describe('File Router', () => {
     it('Should return 400 if no originalname is provided', async () => {
         const { sut } = new SutFactory().create();
         const httpRequest = {
-            file: {},
+            file: {
+                filename: 'any_filename',
+            },
         };
         const httpResponse = await sut.route(httpRequest);
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new MissingParamError('originalname'));
+    });
+    it('Should return 400 if no filename is provided', async () => {
+        const { sut } = new SutFactory().create();
+        const httpRequest = {
+            file: {
+                originalname: 'any_originalname',
+            },
+        };
+        const httpResponse = await sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError('filename'));
     });
 });
