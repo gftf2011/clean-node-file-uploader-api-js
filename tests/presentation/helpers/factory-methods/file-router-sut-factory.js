43b13cfd333618108466348e448ccd4954294f21
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-01-09 15:02:31 -0300

    test: ensure FileRouter returns 500 when FileUploaderUseCase calls crashes

--- tests/presentation/helpers/factory-methods/file-router-sut-factory.js
+++ tests/presentation/helpers/factory-methods/file-router-sut-factory.js
@@ -1,13 +1,27 @@
+const ServerError = require('../../../../src/utils/errors/server-error');
+
 const FileRouter = require('../../../../src/presentation/routers/file-router');
 
 const DependenciesFactory = require('../factories/file-router-dependencies-factory');
 
+const {
+  FILE_ROUTER_SUT_FILE_UPLOADER_USE_CASE_THROWING_ERROR,
+} = require('../constants');
+
 module.exports = class SutFactory {
-  create(_type) {
+  create(type) {
     this.dependencies = new DependenciesFactory().create();
+
+    if (type === FILE_ROUTER_SUT_FILE_UPLOADER_USE_CASE_THROWING_ERROR) {
+      this.dependencies.fileUploaderUseCaseSpy.execute = ({ _name, _path }) => {
+        return Promise.reject(new ServerError());
+      };
+    }
+
     this.sut = new FileRouter({
       fileUploaderUseCase: this.dependencies.fileUploaderUseCaseSpy,
     });
+
     return {
       sut: this.sut,
       ...this.dependencies,
--- tests/presentation/router/file-router.spec.js
+++ tests/presentation/router/file-router.spec.js
@@ -1,6 +1,11 @@
 const SutFactory = require('../helpers/factory-methods/file-router-sut-factory');
 
 const MissingParamError = require('../../../src/utils/errors/missing-param-error');
+const ServerError = require('../../../src/utils/errors/server-error');
+
+const {
+  FILE_ROUTER_SUT_FILE_UPLOADER_USE_CASE_THROWING_ERROR,
+} = require('../helpers/constants');
 
 describe('File Router', () => {
   it('Should return 400 if no originalname is provided', async () => {
@@ -14,6 +19,7 @@ describe('File Router', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('originalname'));
   });
+
   it('Should return 400 if no filename is provided', async () => {
     const { sut } = new SutFactory().create();
     const httpRequest = {
@@ -25,6 +31,7 @@ describe('File Router', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('filename'));
   });
+
   it('Should return 201 when valid credentials are provided', async () => {
     const { sut, fileUploaderUseCaseSpy } = new SutFactory().create();
     fileUploaderUseCaseSpy.file = {
@@ -40,5 +47,26 @@ describe('File Router', () => {
     const httpResponse = await sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(201);
     expect(httpResponse.body).toEqual(fileUploaderUseCaseSpy.file);
+  });
+
+  it('Should return 500 when FileUploaderUseCase calls crashes', async () => {
+    const { sut, fileUploaderUseCaseSpy } = new SutFactory().create(
+      FILE_ROUTER_SUT_FILE_UPLOADER_USE_CASE_THROWING_ERROR,
+    );
+    const httpRequest = {
+      file: {
+        originalname: 'any_originalname',
+        filename: 'any_filename',
+      },
+    };
+    const spyFileUploaderUseCase = jest.spyOn(
+      fileUploaderUseCaseSpy,
+      'execute',
+    );
+    const httpResponse = await sut.route(httpRequest);
+    expect(spyFileUploaderUseCase).toHaveBeenCalled();
+    expect(spyFileUploaderUseCase).toHaveBeenCalledTimes(1);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
   });
 });
