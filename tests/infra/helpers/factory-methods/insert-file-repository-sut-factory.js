90b3b4d15399289e6dfbda2d4f482354a2de5c5d
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-01-22 16:20:20 -0300

    test: ensure InsertFileRepository returns PostgresqlDatabaseError if insertFileDAO throws error

--- tests/infra/helpers/factory-methods/insert-file-repository-sut-factory.js
+++ tests/infra/helpers/factory-methods/insert-file-repository-sut-factory.js
@@ -1,3 +1,5 @@
+const PostgresqlDatabaseError = require('../../../../src/utils/errors/database-error');
+
 const InsertFileRepository = require('../../../../src/infra/repositories/insert-file-repository');
 
 const DependenciesFactory = require('../factories/insert-file-repository-dependencies-factory');
@@ -6,6 +8,7 @@ const {
   INSERT_FILE_REPOSITORY_WITH_NO_DEPENDENCY,
   INSERT_FILE_REPOSITORY_WITH_EMPTY_OBJECT_AS_DEPENDENCY,
   INSERT_FILE_REPOSITORY_HAS_INSERT_FILE_DAO_WITH_NO_INSERT_SINGLE_FILE,
+  INSERT_FILE_REPOSITORY_SUT_INSERT_FILE_DAO_THROWING_ERROR,
 } = require('../constants');
 
 module.exports = class SutFactory {
@@ -24,6 +27,15 @@ module.exports = class SutFactory {
     ) {
       this.sut = new InsertFileRepository({
         insertFileDAO: {},
+      });
+    } else if (
+      type === INSERT_FILE_REPOSITORY_SUT_INSERT_FILE_DAO_THROWING_ERROR
+    ) {
+      this.dependencies.insertFileDAOSpy.insertSingleFile = _values => {
+        return Promise.reject(new PostgresqlDatabaseError());
+      };
+      this.sut = new InsertFileRepository({
+        insertFileDAO: this.dependencies.insertFileDAOSpy,
       });
     } else {
       this.sut = new InsertFileRepository({
--- tests/infra/repositories/insert-file-repository.spec.js
+++ tests/infra/repositories/insert-file-repository.spec.js
@@ -4,11 +4,13 @@ const SutFactory = require('../helpers/factory-methods/insert-file-repository-su
 
 const MissingParamError = require('../../../src/utils/errors/missing-param-error');
 const ServerError = require('../../../src/utils/errors/server-error');
+const PostgresqlDatabaseError = require('../../../src/utils/errors/database-error');
 
 const {
   INSERT_FILE_REPOSITORY_WITH_NO_DEPENDENCY,
   INSERT_FILE_REPOSITORY_WITH_EMPTY_OBJECT_AS_DEPENDENCY,
   INSERT_FILE_REPOSITORY_HAS_INSERT_FILE_DAO_WITH_NO_INSERT_SINGLE_FILE,
+  INSERT_FILE_REPOSITORY_SUT_INSERT_FILE_DAO_THROWING_ERROR,
 } = require('../helpers/constants');
 
 describe('InsertFile Repository', () => {
@@ -117,5 +119,19 @@ describe('InsertFile Repository', () => {
     };
     const promise = sut.insert(request);
     await expect(promise).rejects.toThrow(new ServerError());
+  });
+
+  it('Should return PostgresqlDatabaseError if insertFileDAO throws error', async () => {
+    const { sut } = new SutFactory().create(
+      INSERT_FILE_REPOSITORY_SUT_INSERT_FILE_DAO_THROWING_ERROR,
+    );
+    const fakeName = `${faker.random.word()}.jpg`;
+    const fakePath = `${faker.image.imageUrl()}/${fakeName}`;
+    const request = {
+      name: fakeName,
+      path: fakePath,
+    };
+    const promise = sut.insert(request);
+    await expect(promise).rejects.toThrow(new PostgresqlDatabaseError());
   });
 });
