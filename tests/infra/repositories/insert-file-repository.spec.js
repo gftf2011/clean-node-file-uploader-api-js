24bfb2a7e48ddab358cf4bb2be8d6b630453caa9
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-01-22 14:12:07 -0300

    test: ensure InsertFileRepository returns ServerError if insertFileDAO has no method

--- tests/infra/repositories/insert-file-repository.spec.js
+++ tests/infra/repositories/insert-file-repository.spec.js
@@ -8,6 +8,7 @@ const ServerError = require('../../../src/utils/errors/server-error');
 const {
   INSERT_FILE_REPOSITORY_WITH_NO_DEPENDENCY,
   INSERT_FILE_REPOSITORY_WITH_EMPTY_OBJECT_AS_DEPENDENCY,
+  INSERT_FILE_REPOSITORY_HAS_INSERT_FILE_DAO_WITH_NO_INSERT_SINGLE_FILE,
 } = require('../helpers/constants');
 
 describe('InsertFile Repository', () => {
@@ -48,6 +49,20 @@ describe('InsertFile Repository', () => {
   it('Should return ServerError if dependency is an empty object', async () => {
     const { sut } = new SutFactory().create(
       INSERT_FILE_REPOSITORY_WITH_EMPTY_OBJECT_AS_DEPENDENCY,
+    );
+    const fakeName = `${faker.random.word()}.jpg`;
+    const fakePath = `${faker.image.imageUrl()}/${fakeName}`;
+    const request = {
+      name: fakeName,
+      path: fakePath,
+    };
+    const promise = sut.insert(request);
+    await expect(promise).rejects.toThrow(new ServerError());
+  });
+
+  it('Should return ServerError if insertFileDAO has no method', async () => {
+    const { sut } = new SutFactory().create(
+      INSERT_FILE_REPOSITORY_HAS_INSERT_FILE_DAO_WITH_NO_INSERT_SINGLE_FILE,
     );
     const fakeName = `${faker.random.word()}.jpg`;
     const fakePath = `${faker.image.imageUrl()}/${fakeName}`;
