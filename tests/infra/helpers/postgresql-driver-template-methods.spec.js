4e1d7ad81e5b759d181ec459e4e0bbb0dfdb4b1d
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-01-25 19:33:01 -0300

    test: ensure PostgreSQLDriver disconnects client after create connection

--- tests/infra/helpers/postgresql-driver-template-methods.spec.js
+++ tests/infra/helpers/postgresql-driver-template-methods.spec.js
@@ -8,6 +8,24 @@ const PostgresqlDriverTemplateMethods = require('../../../src/infra/helpers/temp
 const PostgresqlPoolSingleton = require('../../../src/infra/helpers/template-methods/singleton/postgresql-pool-singleton');
 
 describe('PostgreSQL Driver', () => {
+  let fakeIp;
+  let fakePort;
+  let fakeHost;
+  let fakeUser;
+  let fakeDb;
+  let fakePassword;
+  let fakeMax;
+
+  beforeEach(() => {
+    fakeIp = faker.internet.ip();
+    fakePort = faker.internet.port();
+    fakeHost = `${fakeIp}:${fakePort}`;
+    fakeUser = faker.lorem.word(30);
+    fakeDb = faker.lorem.word(30);
+    fakePassword = faker.internet.password(64, false);
+    fakeMax = faker.datatype.number();
+  });
+
   it('Should set connection when variables are provided', () => {
     PostgresqlPoolSingleton.mockImplementation(() => {
       return {
@@ -16,13 +34,6 @@ describe('PostgreSQL Driver', () => {
         },
       };
     });
-    const fakeIp = faker.internet.ip();
-    const fakePort = faker.internet.port();
-    const fakeHost = `${fakeIp}:${fakePort}`;
-    const fakeUser = faker.lorem.word(30);
-    const fakeDb = faker.lorem.word(30);
-    const fakePassword = faker.internet.password(64, false);
-    const fakeMax = faker.datatype.number();
     expect(PostgresqlDriverTemplateMethods.pool).toBeUndefined();
     PostgresqlDriverTemplateMethods.connect({
       host: fakeHost,
@@ -45,13 +56,6 @@ describe('PostgreSQL Driver', () => {
         },
       };
     });
-    const fakeIp = faker.internet.ip();
-    const fakePort = faker.internet.port();
-    const fakeHost = `${fakeIp}:${fakePort}`;
-    const fakeUser = faker.lorem.word(30);
-    const fakeDb = faker.lorem.word(30);
-    const fakePassword = faker.internet.password(64, false);
-    const fakeMax = faker.datatype.number();
     expect(PostgresqlDriverTemplateMethods.pool).toBeUndefined();
     PostgresqlDriverTemplateMethods.connect({
       host: fakeHost,
@@ -81,13 +85,6 @@ describe('PostgreSQL Driver', () => {
         },
       };
     });
-    const fakeIp = faker.internet.ip();
-    const fakePort = faker.internet.port();
-    const fakeHost = `${fakeIp}:${fakePort}`;
-    const fakeUser = faker.lorem.word(30);
-    const fakeDb = faker.lorem.word(30);
-    const fakePassword = faker.internet.password(64, false);
-    const fakeMax = faker.datatype.number();
     PostgresqlDriverTemplateMethods.connect({
       host: fakeHost,
       database: fakeDb,
@@ -104,6 +101,40 @@ describe('PostgreSQL Driver', () => {
     expect(client).not.toBeUndefined();
     expect(spyClientConnect).toHaveBeenCalled();
     expect(spyClientConnect).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should disconnect client after create connection', async () => {
+    PostgresqlPoolSingleton.mockImplementation(() => {
+      return {
+        getInstance: (_host, _database, _user, _password, _port, _max) => {
+          return {
+            connect: jest.fn().mockImplementation(() => ({
+              end: jest.fn(),
+            })),
+          };
+        },
+      };
+    });
+    PostgresqlDriverTemplateMethods.connect({
+      host: fakeHost,
+      database: fakeDb,
+      user: fakeUser,
+      password: fakePassword,
+      port: fakePort,
+      max: fakeMax,
+    });
+    const client = await PostgresqlDriverTemplateMethods.getClientConnect();
+    const spyClientConnect = jest.spyOn(
+      PostgresqlDriverTemplateMethods.pool,
+      'connect',
+    );
+    const spyClientDisconnect = jest.spyOn(client, 'end');
+    expect(client).not.toBeUndefined();
+    expect(spyClientConnect).toHaveBeenCalled();
+    expect(spyClientConnect).toHaveBeenCalledTimes(1);
+    await PostgresqlDriverTemplateMethods.clientDisconnect(client);
+    expect(spyClientDisconnect).toHaveBeenCalled();
+    expect(spyClientDisconnect).toHaveBeenCalledTimes(1);
   });
 
   afterEach(() => {
