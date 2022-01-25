87e9673e7567b5d4039f20a9158561a1f7c06320
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-01-25 01:42:16 -0300

    test: ensure PostgreSQLDriver disconnects from server after set connection

--- tests/infra/helpers/postgresql-driver-template-methods.spec.js
+++ tests/infra/helpers/postgresql-driver-template-methods.spec.js
@@ -8,7 +8,6 @@ const PostgresqlDriverTemplateMethods = require('../../../src/infra/helpers/temp
 const PostgresqlPoolSingleton = require('../../../src/infra/helpers/template-methods/singleton/postgresql-pool-singleton');
 
 describe('PostgreSQL Driver', () => {
-  // eslint-disable-next-line jest/expect-expect
   it('Should set connection when variables are provided', () => {
     PostgresqlPoolSingleton.mockImplementation(() => {
       return {
@@ -34,5 +33,46 @@ describe('PostgreSQL Driver', () => {
       max: fakeMax,
     });
     expect(PostgresqlDriverTemplateMethods.pool).not.toBeUndefined();
+  });
+
+  it('Should disconnect from server after set connection', async () => {
+    PostgresqlPoolSingleton.mockImplementation(() => {
+      return {
+        getInstance: (_host, _database, _user, _password, _port, _max) => {
+          return {
+            end: jest.fn(),
+          };
+        },
+      };
+    });
+    const fakeIp = faker.internet.ip();
+    const fakePort = faker.internet.port();
+    const fakeHost = `${fakeIp}:${fakePort}`;
+    const fakeUser = faker.lorem.word(30);
+    const fakeDb = faker.lorem.word(30);
+    const fakePassword = faker.internet.password(64, false);
+    const fakeMax = faker.datatype.number();
+    expect(PostgresqlDriverTemplateMethods.pool).toBeUndefined();
+    PostgresqlDriverTemplateMethods.connect({
+      host: fakeHost,
+      database: fakeDb,
+      user: fakeUser,
+      password: fakePassword,
+      port: fakePort,
+      max: fakeMax,
+    });
+    expect(PostgresqlDriverTemplateMethods.pool).not.toBeUndefined();
+    await PostgresqlDriverTemplateMethods.disconnect();
+    const spyDisconnect = jest.spyOn(
+      PostgresqlDriverTemplateMethods.pool,
+      'end',
+    );
+    expect(spyDisconnect).toHaveBeenCalled();
+    expect(spyDisconnect).toHaveBeenCalledTimes(1);
+  });
+
+  afterEach(() => {
+    PostgresqlDriverTemplateMethods.pool = undefined;
+    PostgresqlDriverTemplateMethods.client = undefined;
   });
 });
