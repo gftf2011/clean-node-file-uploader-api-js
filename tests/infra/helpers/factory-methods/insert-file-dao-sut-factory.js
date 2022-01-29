02189bcf6519a05b0592dd0f8d0c290aa8514b4e
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-01-29 07:21:30 -0300

    test: ensure InsertFileDAO returns null if single transaction throws error

--- tests/infra/helpers/factory-methods/insert-file-dao-sut-factory.js
+++ tests/infra/helpers/factory-methods/insert-file-dao-sut-factory.js
@@ -2,14 +2,29 @@ const InsertFileDAO = require('../../../../src/infra/helpers/dao/insert-file-dao
 
 const DependenciesFactory = require('../factories/insert-file-dao-dependencies-factory');
 
+const {
+  INSERT_FILE_DAO_SINGLE_TRANSACTION_SUT_THROWING_ERROR,
+} = require('../constants');
+
 module.exports = class SutFactory {
-  create(_type) {
+  create(type) {
     this.dependencies = new DependenciesFactory().create();
 
-    this.sut = new InsertFileDAO({
-      databaseDriverTemplateMethods:
-        this.dependencies.databaseDriverTemplateMethodsSpy,
-    });
+    if (type === INSERT_FILE_DAO_SINGLE_TRANSACTION_SUT_THROWING_ERROR) {
+      this.dependencies.databaseDriverTemplateMethodsSpy.singleTransaction =
+        async (_client, _statement, _values) => {
+          return Promise.reject(new Error());
+        };
+      this.sut = new InsertFileDAO({
+        databaseDriverTemplateMethods:
+          this.dependencies.databaseDriverTemplateMethodsSpy,
+      });
+    } else {
+      this.sut = new InsertFileDAO({
+        databaseDriverTemplateMethods:
+          this.dependencies.databaseDriverTemplateMethodsSpy,
+      });
+    }
 
     return {
       sut: this.sut,
--- tests/infra/helpers/insert-file-dao.spec.js
+++ tests/infra/helpers/insert-file-dao.spec.js
@@ -1,6 +1,12 @@
 const faker = require('faker');
 
+const PostgresqlDatabaseError = require('../../../src/utils/errors/database-error');
+
 const SutFactory = require('./factory-methods/insert-file-dao-sut-factory');
+
+const {
+  INSERT_FILE_DAO_SINGLE_TRANSACTION_SUT_THROWING_ERROR,
+} = require('./constants');
 
 class MockClient {
   constructor({ properties } = {}) {
@@ -28,5 +34,39 @@ describe('InsertFile DAO', () => {
     expect(databaseDriverTemplateMethodsSpy.clientDisconnection).toEqual(
       new MockClient(),
     );
+  });
+
+  it('Should return PostgresqlDatabaseError if client is undefined', async () => {
+    const { sut, databaseDriverTemplateMethodsSpy } = new SutFactory().create();
+    const fakeId = faker.datatype.uuid();
+    const fakeName = `${faker.random.word()}.jpg`;
+    const fakePath = `${faker.image.imageUrl()}/${fakeName}`;
+    databaseDriverTemplateMethodsSpy.response = {
+      id: fakeId,
+      name: fakeName,
+      path: fakePath,
+    };
+    const promise = sut.insertSingleFile(['name', 'path']);
+    await expect(promise).rejects.toThrow(new PostgresqlDatabaseError());
+  });
+
+  it('Should return null if single transaction throws error', async () => {
+    const { sut, databaseDriverTemplateMethodsSpy } = new SutFactory().create(
+      INSERT_FILE_DAO_SINGLE_TRANSACTION_SUT_THROWING_ERROR,
+    );
+    const fakeId = faker.datatype.uuid();
+    const fakeName = `${faker.random.word()}.jpg`;
+    const fakePath = `${faker.image.imageUrl()}/${fakeName}`;
+    databaseDriverTemplateMethodsSpy.clientConnection = new MockClient();
+    databaseDriverTemplateMethodsSpy.response = {
+      id: fakeId,
+      name: fakeName,
+      path: fakePath,
+    };
+    const response = await sut.insertSingleFile(['name', 'path']);
+    expect(databaseDriverTemplateMethodsSpy.clientRollback).toEqual(
+      new MockClient(),
+    );
+    expect(response).toBeNull();
   });
 });
