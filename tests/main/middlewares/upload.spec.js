06616f46169d3b4dc5f0ef1c0333f529e7d85dba
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-01-30 17:26:32 -0300

    test: ensure Upload Middleware uploads JPEG file

--- tests/main/middlewares/upload.spec.js
+++ tests/main/middlewares/upload.spec.js
@@ -137,4 +137,46 @@ describe('Upload Middleware', () => {
       path.basename(filePath),
     );
   });
+
+  it('Should upload JPEG file', async () => {
+    const dirPath = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'temp',
+      'uploads',
+    );
+    const filePath = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'public',
+      'test',
+      'test-image.jpeg',
+    );
+
+    server.post(
+      '/api/test-file-parser',
+      express.static(dirPath),
+      upload.single('file'),
+      (req, res) => {
+        const { originalname, filename } = req.file;
+        res.json({ originalname, filename });
+      },
+    );
+    const response = await request(server)
+      .post('/api/test-file-parser')
+      .attach('file', filePath)
+      .set('Content-Type', 'multipart/form-data');
+
+    expect(response.status).toBe(200);
+    expect(
+      fs.existsSync(path.join(dirPath, JSON.parse(response.text).filename)),
+    ).toBe(true);
+    expect(JSON.parse(response.text).originalname).toBe(
+      path.basename(filePath),
+    );
+  });
 });
