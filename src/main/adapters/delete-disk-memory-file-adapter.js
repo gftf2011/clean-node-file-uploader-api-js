d5015863e257de21cc26028c73c2f9f53b87b713
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-02-06 01:40:32 -0300

    test: ensure File Routes returns 201 when valid JPG file is provided

--- src/main/adapters/delete-disk-memory-file-adapter.js
+++ src/main/adapters/delete-disk-memory-file-adapter.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
+const { resolve } = require('path');
 
 const FileNotFoundError = require('../../utils/errors/file-not-found-error');
 
 module.exports = class DeleteDiskMemoryFileAdapter {
   async delete(path) {
-    if (fs.existsSync(path)) {
-      await fs.unlink(path);
+    if (fs.existsSync(resolve('..', '..', '..', 'temp', 'uploads', path))) {
+      await fs.unlink(resolve('..', '..', '..', 'temp', 'uploads', path));
     } else {
       throw new FileNotFoundError(path);
     }
