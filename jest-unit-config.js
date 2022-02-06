09030a6deeb95c0308e298aeb41559a109f78fe0
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-02-06 20:22:06 -0300

    chore: add ignore patterns to jest unit tests

--- jest-unit-config.js
+++ jest-unit-config.js
@@ -1,4 +1,12 @@
 const config = require('./jest.config');
 
 config.testMatch = ['**/*.spec.js'];
+config.coveragePathIgnorePatterns = [
+  'node_modules',
+  '<rootDir>/src/main/index.js',
+  '<rootDir>/src/main/adapters',
+  '<rootDir>/src/main/bootstrap',
+  '<rootDir>/src/main/composers',
+  '<rootDir>/src/main/config/routes.js',
+];
 module.exports = config;
