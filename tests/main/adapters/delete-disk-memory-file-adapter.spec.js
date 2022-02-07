02876bb7b6b4a5c0a5e26d10828c39e32f5e4deb
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-02-07 05:27:46 -0300

    test: ensure DeleteDiskMemoryFile Adapter throws error if file does not exists

--- tests/main/adapters/delete-disk-memory-file-adapter.spec.js
+++ tests/main/adapters/delete-disk-memory-file-adapter.spec.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 
 const SutFactory = require('./helpers/factories/delete-disk-memory-file-adapter-factory');
 
+const FileNotFoundError = require('../../../src/utils/errors/file-not-found-error');
+
 describe('DeleteDiskMemoryFile Adapter', () => {
   it('Should delete file if exists', async () => {
     const src = path.resolve(
@@ -32,5 +34,14 @@ describe('DeleteDiskMemoryFile Adapter', () => {
     await sut.delete('test-image.png');
 
     expect(fs.existsSync(dest)).toBe(false);
+  });
+
+  it('Should throw error if file does not exists', async () => {
+    const { sut } = new SutFactory().create();
+    const promise = sut.delete('test-image.png');
+
+    await expect(promise).rejects.toThrow(
+      new FileNotFoundError('test-image.png'),
+    );
   });
 });
