3e0b010fd1f27159069b48b4cee849491d0c6ab7
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-04-10 23:43:15 -0300

    feat: create node cluster

--- src/main/index.js
+++ src/main/index.js
@@ -1,17 +1,23 @@
 require('./bootstrap');
 
+const cluster = require('cluster');
+const os = require('os');
 const server = require('./server');
 
 const loader = require('./loader');
 const routes = require('./config/routes');
 
-loader()
-  .then(() => {
-    routes(server);
-    server.listen(parseInt(process.env.APPLICATION_PORT, 10), () => {
-      console.log('Server Running');
+if (cluster.isPrimary && !+process.env.ENABLE_BENCHMARK) {
+  os.cpus().forEach(() => cluster.fork());
+} else {
+  loader()
+    .then(() => {
+      routes(server);
+      server.listen(parseInt(process.env.APPLICATION_PORT, 10), () => {
+        console.log('Server Running');
+      });
+    })
+    .catch(error => {
+      console.error(error);
     });
-  })
-  .catch(error => {
-    console.error(error);
-  });
+}
